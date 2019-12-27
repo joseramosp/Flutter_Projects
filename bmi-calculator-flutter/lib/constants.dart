a63b215f02018331b1e97833ec469b831d1ecbbb
Jose Ramos <jmrp.04@gmail.com>
2019-12-26 19:16:42 -0500

    The bmi is 60% done

---
+++ bmi-calculator-flutter/lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xff1d1e33);
+const kInactiveCardColor = Color(0xff111328);
+const kInactiveTextColor = Color(0xff8d8e98);
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: kInactiveTextColor,
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
